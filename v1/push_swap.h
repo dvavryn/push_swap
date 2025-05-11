5267b8264773cb4bafc94332591ce8359f875826
dvavryn <vavryn.dominic@outlook.com>
2025-05-12 00:08:54 +0200

    init

---
+++ v1/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/11 22:34:20 by dvavryn           #+#    #+#             */
+/*   Updated: 2025/05/11 23:13:09 by dvavryn          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+// read, write
+# include <unistd.h>
+// malloc, free
+# include <stdlib.h>
+// libft + ft_printf
+# include "libft.h"
+
+#endif
