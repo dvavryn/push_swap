11460992e17dd15bfbec4f3e98ce3517a4007f5d
dvavryn <vavryn.dominic@outlook.com>
2025-05-15 23:46:07 +0200

    love it <3

--- <3 love it/c/srcs/steps.c
+++ <3 love it/c/srcs/steps.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 21:51:37 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/05/15 23:08:40 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/05/15 23:43:00 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,59 @@
 
 void steps(int *a, int *b, size_t size)
 {
+/* 	help_print(a, b ,size);
+	push(a, b, size);
+	push(a, b, size);
+	// push(a, b, size);
+	help_print(a, b ,size);
+	rotate(a, size);
+	help_print(a, b ,size);
+	rrotate(a, size);
+	help_print(a, b ,size);
+ */
 	help_print(a, b, size);
+
 	swap(a);
-	// help_print(a, b, size);
-	push(a, b, size);
-	// help_print(a, b, size);
-	push(a, b, size);
-	// help_print(a, b, size);
-	rotate(a, size);	
+	printf("\nsa\n");
 	help_print(a, b, size);
-	rrotate(a, size);	
+	
+	push(a, b, size);
+	printf("\npb\n");
 	help_print(a, b, size);
-	// rrotate(b, size);	
-	// help_print(a, b, size);
+	
+	push(a, b, size);
+	printf("\npb\n");
+	help_print(a, b, size);
+	
+	push(a, b, size);
+	printf("\npb\n");
+	help_print(a, b, size);
+	
+	rr(a, b, size);
+	printf("\nrr\n");
+	help_print(a, b, size);
+	
+	rrr(a, b, size);
+	printf("\nrrr\n");
+	help_print(a, b, size);
+	
+	swap(a);
+	printf("\nsa\n");
+	help_print(a, b, size);
+	
+	push(b, a, size);
+	printf("\npa\n");
+	help_print(a, b, size);
+	
+	push(b, a, size);
+	printf("\npa\n");
+	help_print(a, b, size);
+	
+	push(b, a, size);
+	printf("\npa\n");
+	help_print(a, b, size);
 }
+
 
 
 /* 
@@ -37,7 +76,7 @@ sb	||	swap(b);
 ss	||	ss(a, b);
 
 pa	||	push(a, b, size);
-pb	||	push(b, a, size);
+pb	||	push(a, b, size);
 
 ra	||	rotate(a, size);
 rb	||	rotate(b, size);
