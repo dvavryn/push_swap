f8dd2193dc0dd375490aecdab8bcc95a82bd8a7f
Dominic Vavryn <dvavryn@c3r2p5.42vienna.com>
2025-06-02 17:34:34 +0200

    changes

--- step_2.c
+++ step_2.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 22:52:44 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/06/02 00:54:59 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/06/02 11:01:21 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,51 +30,51 @@ void	step_two(int *a, size_t size)
 	}
 	stacks.sa = size;
 	stacks.sb = 0;
-	solve_0(&(*stacks));
+	// solve_0(&(*stacks));
 	free(stacks.b);
 }
 
-static void	solve_0(t_data *stacks)
-{
-	if (stacks->sa <= 3)
-		solve_fixed(stacks);
-	else
-		algo(&(*stacks));
-}
-static void	solve_fixed(t_data *stacks)
-{
-	if (stacks->sa == 2)
-		fix_2(&(*stacks));
-	else if (stacks->sa == 3)
-		fix_3(&(*stacks)->a);
-}
+// static void	solve_0(t_data *stacks)
+// {
+// 	if (stacks->sa <= 3)
+// 		solve_fixed(stacks);
+// 	else
+// 		algo(&(*stacks));
+// }
+// static void	solve_fixed(t_data *stacks)
+// {
+// 	if (stacks->sa == 2)
+// 		fix_2(&(*stacks));
+// 	else if (stacks->sa == 3)
+// 		fix_3(&(*stacks)->a);
+// }
 
-static void	fix_2(t_data *stacks)
-{
-	if (stacks->a[0] < stacks->a[1])
-		sa(stacks->a);
-}
+// static void	fix_2(t_data *stacks)
+// {
+// 	if (stacks->a[0] < stacks->a[1])
+// 		sa(stacks->a);
+// }
 
-static void	fix_3(t_data *stacks)
-{
-	if (stacks->a[0] < stacks->a[1] && stacks->a[1] > stacks->a[2]
-		&& stacks->a[0] < stacks->a[2])
-	{
-		ra(&(*stacks));
-		sa(&(*stacks));
-	}
-	else if (stacks->a[0] > stacks->a[1] && stacks->a[1] < stacks->a[2]
-		&& stacks->a[0] < stacks->a[2])
-		sa(&(*stacks));
-	else if (stacks->a[0] < stacks->a[1] && stacks->a[1] > stacks->a[2]
-		&& stacks->a[0] > stacks->a[2])
-		rra(&(*stacks));
-	else if (stacks->a[0] > stacks->a[1] && stacks->a[1] < stacks->a[2]
-		&& stacks->a[0] > stacks->a[2])
-		ra(&(*stacks));
-	else if (stacks->a[0] > stacks->a[1] && stacks->a[1] > stacks->a[2])
-	{
-		ra(&(*stacks));
-		sa(&(*stacks));
-	}
-}
+// static void	fix_3(t_data *stacks)
+// {
+// 	if (stacks->a[0] < stacks->a[1] && stacks->a[1] > stacks->a[2]
+// 		&& stacks->a[0] < stacks->a[2])
+// 	{
+// 		ra(&(*stacks));
+// 		sa(&(*stacks));
+// 	}
+// 	else if (stacks->a[0] > stacks->a[1] && stacks->a[1] < stacks->a[2]
+// 		&& stacks->a[0] < stacks->a[2])
+// 		sa(&(*stacks));
+// 	else if (stacks->a[0] < stacks->a[1] && stacks->a[1] > stacks->a[2]
+// 		&& stacks->a[0] > stacks->a[2])
+// 		rra(&(*stacks));
+// 	else if (stacks->a[0] > stacks->a[1] && stacks->a[1] < stacks->a[2]
+// 		&& stacks->a[0] > stacks->a[2])
+// 		ra(&(*stacks));
+// 	else if (stacks->a[0] > stacks->a[1] && stacks->a[1] > stacks->a[2])
+// 	{
+// 		ra(&(*stacks));
+// 		sa(&(*stacks));
+// 	}
+// }
